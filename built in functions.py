e59d1926c56e9c8ccf8e668716976b4c5fdf2702
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-19 20:42:44 +0000

    type of built in functions

--- built in functions.py
+++ built in functions.py
@@ -1,1 +1,12 @@
-kjbkbb
+int
+str
+sum
+print
+range
+ # Built-in functions are just some that come with Python. We can use them anywhere in our programs without defining them or telling Python where they come from.
+
+# There's quite a few built-in functions that we can use—and they do a variety of things. Here's a few more examples:
+
+#len(my_list) # gives you the length of the list my_list —it also works with tuples and strings;
+#max(my_list) # gives you the maximum number in my_list  (there's also min(my_list) )
+#round(my_decimal) # rounds my_decimal  according to maths rules (e.g. 3.5 gets rounded up to 4; 3.49 gets rounded down to 3)

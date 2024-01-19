ba251435c51f686310d95374af3c94d42cfa7db5
beniewaku <78078820+beniewaku@users.noreply.github.com>
2024-01-19 20:56:15 +0000

    quiz

---
+++ object oriented quiz.py
@@ -0,0 +1,10 @@
+# how many objects of type Car do we have at the end of this code segment
+
+class Car:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+
+fiesta = Car('ford', 'fiesta')
+focus = Car('ford', 'focus')
